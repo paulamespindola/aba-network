2c40adf52bee94512df0876c35c33cd20bbd8cbd
Kennedy Pegorara <pegorara@proton.me>
2024-09-23 23:21:50 -0300

    Add association between Company and User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,16 @@
 class User < ApplicationRecord
-
+  belongs_to :company
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validate :email_domain_check
   validates :password, presence: true, length: { minimum: 8 }
   validate :password_complexity
 
- 
   enum role: { user: 0, moderator: 1 }
 
   private
@@ -22,7 +21,7 @@ class User < ApplicationRecord
       errors.add(:email, "deve ser de um domínio válido (#{allowed_domain})")
     end
   end
-  
+
   def password_complexity
     return if password.blank?
     unless password.match(/\A(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).+\z/)
